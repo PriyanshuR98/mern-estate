c16443ce2767e11ca0882eae307c2a74c3cd9108
Priyanshu <codingalley62@gmail.com>
2023-10-14 10:37:59 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+// mongoose.connect("mongodb+srv://priyanshu:priyanshu@mern-estate.hy1bmxt.mongodb.net/?retryWrites=true&w=majority")
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connect to mongoDB!!!")
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
+
 
 const app=express();
 
